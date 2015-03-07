f20fba16bdb32deffd96a7db33167266c1139bd3
Peter WONG <luiges90@gmail.com>
2015-03-07 12:08:38 +0800

    錯誤LOG

--- desktop/src/com/zhsan/desktop/DesktopLauncher.java
+++ desktop/src/com/zhsan/desktop/DesktopLauncher.java
@@ -4,9 +4,31 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.zhsan.ZHSan2;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
         System.setProperty("user.home", ".");
+
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.err.println("Uncaught exception on thread: " + t.getName());
+                e.printStackTrace(System.err);
+
+                String date = new SimpleDateFormat("yyyy-MM-dd HHmmss").format(Calendar.getInstance().getTime());
+                try (PrintWriter w = new PrintWriter(new FileWriter("Crash_" + date + ".log"))) {
+                    w.println("Uncaught exception on thread: " + t.getName());
+                    e.printStackTrace(w);
+                } catch (Exception e2) {
+
+                }
+            }
+        });
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "";
