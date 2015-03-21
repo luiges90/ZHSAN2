e49cf2abaf444bbd5a643ccb7867f8e9444573ce
Peter WONG <luiges90@gmail.com>
2015-03-21 19:35:19 +0800

    HC/EQ/TS for Point

--- core/src/com/zhsan/common/Point.java
+++ core/src/com/zhsan/common/Point.java
@@ -18,4 +18,32 @@ public final class Point {
                 Integer.parseInt(t[1])
         );
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        if (y != point.y) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
