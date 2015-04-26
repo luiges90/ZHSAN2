212c3d2c0487f534c823702235bf819a019c24bc
Peter WONG <luiges90@gmail.com>
2015-04-26 17:20:48 +0800

    修正地圖捲動

--- core/src/com/zhsan/gamecomponents/MapLayer.java
+++ core/src/com/zhsan/gamecomponents/MapLayer.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
@@ -501,6 +502,12 @@ public class MapLayer extends WidgetGroup {
     }
 
     private class InputEventListener extends InputListener {
+
+        @Override
+        public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+            moveStateX = MoveStateX.IDLE;
+            moveStateY = MoveStateY.IDLE;
+        }
 
         @Override
         public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
